ddc1146cd14a13a960646b6d0ce46ffc797e7304
infinigon <infinigon@demigod.bar>
2024-09-26 21:54:48 +0100

    merge always on cpu

--- pytorch/merge.py
+++ pytorch/merge.py
@@ -7,7 +7,7 @@ import sys
 basedir = os.path.dirname(os.path.realpath(__file__))
 
 model = torch.jit.load(os.path.join(basedir, 'model.pt'))
-state = torch.load(sys.argv[1], weights_only=True)
+state = torch.load(sys.argv[1], weights_only=True, map_location=torch.device('cpu'))
 model.load_state_dict(state)
 
 model.save(os.path.join(basedir, 'merged.pt'))
