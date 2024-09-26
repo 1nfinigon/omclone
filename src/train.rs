3019072edc6d49f9166c6fda8e33807a86009a58
infinigon <infinigon@demigod.bar>
2024-09-26 19:35:34 +0100

    ignore errors, i will check later idk

--- src/train.rs
+++ src/train.rs
@@ -7,8 +7,10 @@ mod search_state;
 mod sim;
 mod utils;
 
+use eyre::{eyre, Result};
 use num_traits::ToPrimitive;
 use rand::prelude::*;
+use sim::BasicInstr;
 use std::fs::File;
 use std::io::{BufReader, BufWriter};
 use std::path::{Path, PathBuf};
@@ -16,9 +18,99 @@ use tch;
 use uuid;
 
 #[cfg(feature = "color_eyre")]
-use color_eyre::{eyre::Result, install};
+use color_eyre::install;
 #[cfg(not(feature = "color_eyre"))]
-use simple_eyre::{eyre::Result, install};
+use simple_eyre::install;
+
+fn process_one_solution(
+    fpath: impl AsRef<Path>,
+    rng: &mut impl Rng,
+    puzzle_map: &utils::PuzzleMap,
+    all_tensors: &mut Vec<(
+        Box<nn::Features>,
+        f32,
+        [f32; BasicInstr::N_TYPES],
+        (usize, usize),
+    )>,
+) -> Result<()> {
+    let solution = parser::parse_solution(&mut BufReader::new(File::open(fpath.as_ref())?))?;
+
+    let (_, puzzle) = puzzle_map.get(&solution.puzzle_name).unwrap();
+    let history_fpath = format!(
+        "{}.history",
+        fpath
+            .as_ref()
+            .to_str()
+            .unwrap()
+            .strip_suffix(".solution")
+            .unwrap()
+    );
+    let history_file =
+        search_history::HistoryFile::read(&mut BufReader::new(File::open(&history_fpath)?))?;
+
+    let init = parser::puzzle_prep(puzzle, &solution)?;
+    let world = sim::WorldWithTapes::setup_sim(&init)?;
+
+    let mut search_state =
+        search_state::State::new(world.world, history_file.timestep_limit as u64);
+
+    let mut tensors = Vec::new();
+
+    for history_item in history_file.history.0.iter() {
+        let arm_index = search_state.next_arm_index();
+        let instr =
+            world.tapes[arm_index].get(search_state.world.timestep as usize, world.repeat_length);
+
+        let next_arm_pos = search_state.world.arms[arm_index].pos;
+        let (x, y) = nn::features::normalize_position(next_arm_pos).expect("arm out of nn bounds");
+
+        match history_item.kind {
+            search_history::Kind::FromOptimalSolution => {
+                // include 10% of these
+                if rng.gen_bool(0.1) {
+                    // mock up a policy of solely picking the right answer, 90% of the time
+                    // TODO: dunno if this is right/good,
+                    // it certainly gets us more data quicker though.
+                    let mut visits = [1f32; sim::BasicInstr::N_TYPES];
+                    visits[instr.to_usize().unwrap()] = 9.;
+                    tensors.push((search_state.nn_features.clone(), visits, (x, y)));
+                }
+            }
+            search_history::Kind::MCTS => {
+                // include 100% of these < 200 playouts, and 100% of these
+                // >= 200 playouts
+                let n_playouts: u32 = history_item.playouts.iter().sum();
+                let _ = n_playouts;
+                let visits: Vec<_> = history_item
+                    .playouts
+                    .iter()
+                    .copied()
+                    .map(|p| p as f32)
+                    .collect();
+                tensors.push((
+                    search_state.nn_features.clone(),
+                    visits.try_into().unwrap(),
+                    (x, y),
+                ));
+            }
+        }
+
+        search_state.update(instr);
+    }
+
+    let final_result = if let Some(final_result) = search_state.evaluate_final_state() {
+        final_result
+    } else {
+        return Err(eyre!(
+            "after playing out the solution, did not reach a final state. Ignoring"
+        ));
+    };
+
+    for (input, visits, pos) in tensors {
+        all_tensors.push((input, final_result, visits, pos));
+    }
+    Ok(())
+}
 
 fn main() -> Result<()> {
     std::env::set_var("RUST_BACKTRACE", "full");
@@ -41,80 +133,15 @@ fn main() -> Result<()> {
     };
     utils::read_file_suffix_recurse(&mut cb, ".solution", "test/current-epoch");
 
-    for fpath in solution_paths {
-        let solution = parser::parse_solution(&mut BufReader::new(File::open(&fpath)?))?;
-
-        println!("{}", solution.puzzle_name);
-        let (_, puzzle) = puzzle_map.get(&solution.puzzle_name).unwrap();
-        let history_fpath = format!(
-            "{}.history",
-            fpath.to_str().unwrap().strip_suffix(".solution").unwrap()
-        );
-        let history_file =
-            search_history::HistoryFile::read(&mut BufReader::new(File::open(&history_fpath)?))?;
-
-        let init = parser::puzzle_prep(puzzle, &solution)?;
-        let world = sim::WorldWithTapes::setup_sim(&init)?;
-
-        let mut search_state =
-            search_state::State::new(world.world, history_file.timestep_limit as u64);
-
-        let mut tensors = Vec::new();
-
-        for history_item in history_file.history.0.iter() {
-            let arm_index = search_state.next_arm_index();
-            let instr = world.tapes[arm_index]
-                .get(search_state.world.timestep as usize, world.repeat_length);
-
-            let next_arm_pos = search_state.world.arms[arm_index].pos;
-            let (x, y) =
-                nn::features::normalize_position(next_arm_pos).expect("arm out of nn bounds");
-
-            match history_item.kind {
-                search_history::Kind::FromOptimalSolution => {
-                    // include 10% of these
-                    if rng.gen_bool(0.1) {
-                        // mock up a policy of solely picking the right answer, 90% of the time
-                        // TODO: dunno if this is right/good,
-                        // it certainly gets us more data quicker though.
-                        let mut visits = [1f32; sim::BasicInstr::N_TYPES];
-                        visits[instr.to_usize().unwrap()] = 9.;
-                        tensors.push((search_state.nn_features.clone(), visits, (x, y)));
-                    }
-                }
-                search_history::Kind::MCTS => {
-                    // include 100% of these < 200 playouts, and 100% of these
-                    // >= 200 playouts
-                    let n_playouts: u32 = history_item.playouts.iter().sum();
-                    let _ = n_playouts;
-                    let visits: Vec<_> = history_item
-                        .playouts
-                        .iter()
-                        .copied()
-                        .map(|p| p as f32)
-                        .collect();
-                    tensors.push((
-                        search_state.nn_features.clone(),
-                        visits.try_into().unwrap(),
-                        (x, y),
-                    ));
-                }
+    for (i, fpath) in solution_paths.iter().enumerate() {
+        println!("{}/{}", i, solution_paths.len());
+        let result = process_one_solution(fpath, rng, &puzzle_map, &mut all_tensors);
+        match result {
+            Ok(()) => (),
+            Err(e) => {
+                // TODO: figure out where these errors are coming from???????
+                println!("ignoring error for {:?}: {}", fpath, e)
             }
-
-            search_state.update(instr);
-        }
-
-        let final_result = if let Some(final_result) = search_state.evaluate_final_state() {
-            final_result
-        } else {
-            println!(
-                "WARNING: after playing out the solution, did not reach a final state. Ignoring"
-            );
-            continue;
-        };
-
-        for (input, visits, pos) in tensors {
-            all_tensors.push((input, final_result, visits, pos));
         }
     }
 
