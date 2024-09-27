dba6b0eb3b9ab29e4203d7afa2de1951b281d04e
infinigon <infinigon@demigod.bar>
2024-09-27 08:05:16 +0100

    this api is great

--- pytorch/common.py
+++ pytorch/common.py
@@ -8,13 +8,13 @@ def device():
         return torch.device('mps')
     if torch.cuda.is_available():
         print("Using CUDA")
-        min_i, min_d, min_w = None, None, None
+        min_i, min_w = None, None
         for i in range(torch.cuda.device_count()):
             d = torch.cuda.device(i)
             w = torch.cuda.power_draw(d)
             if min_d is None or w < min_w:
-                min_i, min_d, min_w = i, d, w
+                min_i, min_w = i, w
         print("Selected device ", min_i)
-        return min_d
+        return torch.device('cuda:{}'.format(min_i))
     print("Using CPU")
     return torch.device('cpu')
