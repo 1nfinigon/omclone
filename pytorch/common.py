473f13f3a45d611b6d3a8d0aca2bfb830609388c
infinigon <infinigon@demigod.bar>
2024-09-27 07:53:57 +0100

    cuda autoselect device hack

--- pytorch/common.py
+++ pytorch/common.py
@@ -8,6 +8,13 @@ def device():
         return torch.device('mps')
     if torch.cuda.is_available():
         print("Using CUDA")
-        return torch.device('cuda')
+        min_d, min_w = None, None
+        for i in range(torch.cuda.device_count()):
+            d = torch.cuda.device(i)
+            w = torch.cuda.power_draw(d)
+            if min_d is None or w < min_w:
+                min_d, min_d = d, w
+        print("Selected device ", min_d)
+        return min_d
     print("Using CPU")
     return torch.device('cpu')
