d4497a81abf04ed891f70778e1a4127d80f94de3
infinigon <infinigon@demigod.bar>
2024-09-29 11:33:23 +0100

    fix setup

--- setup.sh
+++ setup.sh
@@ -3,18 +3,19 @@
 set -e -u -x -o pipefail
 
 pushd ..
-git clone https://github.com/F43nd1r/om-leaderboard.git
-git clone https://github.com/ianh/omsim.git
+[ ! -d om-leaderboard ] && git clone https://github.com/F43nd1r/om-leaderboard.git
+[ ! -d omsim ] && git clone https://github.com/ianh/omsim.git
 popd
 
 mkdir -p test
 pushd test
 this_path="$(pwd)"
-ln -s ../omsim/test/puzzle .
-ln -s ../omsim/test/solution .
-ln -s ../om-leaderboard ./om-leaderboard-master
+ln -sf ../../omsim/test/puzzle .
+ln -sf ../../omsim/test/solution .
+ln -sf ../../om-leaderboard ./om-leaderboard-master
 popd
 
+rm src/default_paths.txt
 echo "$this_path" >> src/default_paths.txt
 echo "" >> src/default_paths.txt
 echo "" >> src/default_paths.txt
