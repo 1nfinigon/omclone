1nfinigon/omclone

commit fbb7916e42f880a056c026e4e9ab1bb6e48fbbd3

formalize Rot and RawRot types; fix revgen opp bug

src/render_sim.rs
@@ -9,8 +9,8 @@ pub fn pos_to_xy(input: &Pos) -> GFXPos {
     let b = input.y as f32;
     [a * 2. + b, b * f32::sqrt(3.)]
 }
-pub fn rot_to_angle(r: Rot) -> f32 {
-    (r as f32) * PI / 3.
+pub fn rot_to_angle(r: RawRot) -> f32 {
+    (r.0 as f32) * PI / 3.
 }
 
 //note: 1 hex has inner radius of 1 (width of 2).
@@ -431,7 +431,7 @@ impl RenderDataBase {
                 let bond = atom.connections[r];
                 for (bondtype, bindtype) in matches {
                     if bond.intersects(bondtype) {
-                        let angle = rot_to_angle(r as Rot) + atom.rot;
+                        let angle = rot_to_angle(RawRot(r as i32)) + atom.rot;
                         ctx.apply_bindings(bindtype);
                         ctx.apply_uniforms(UniformsSource::table(&UvUniforms {
                             offset,
@@ -626,8 +626,8 @@ impl RenderDataBase {
             let color = [0., 0., 0.];
             let offset = [f_arm.pos.x, f_arm.pos.y];
             let triangles_drawn = if f_arm.grabbing { 6 } else { 3 };
-            for r in (0..6).step_by(f_arm.arm_type.angles_between_arm() as usize) {
-                let angle = f_arm.rot + rot_to_angle(r);
+            for r in Rot::step_by(f_arm.arm_type.angles_between_arm()) {
+                let angle = f_arm.rot + rot_to_angle(r.raw());
                 ctx.apply_uniforms(UniformsSource::table(&BasicUniforms {
                     color,
                     offset,

src/revgen.rs
@@ -62,8 +62,8 @@ fn gen_molecule(rng: &mut impl Rng, size: usize) -> AtomPattern {
             let prev = self.occupied.insert(pos, pattern_idx);
             assert!(prev.is_none());
             self.adjacencies.swap_remove(&pos);
-            for r in 0..6 {
-                let pos = pos + rot_to_pos(r);
+            for r in Rot::ALL {
+                let pos = pos + r.to_pos();
                 if let Some(other_idx) = self.occupied.get(&pos).copied() {
                     self.possible_bonds.push((pattern_idx, other_idx));
                 } else {
@@ -116,11 +116,11 @@ fn gen_molecule(rng: &mut impl Rng, size: usize) -> AtomPattern {
             continue;
         }
         let delta_1to2 = gen.pattern[idx2].pos - gen.pattern[idx1].pos;
-        let r_1to2 = pos_to_rot(delta_1to2).unwrap();
-        let r_2to1 = pos_to_rot(-delta_1to2).unwrap();
+        let r_1to2 = Rot::from_unit_pos(delta_1to2).unwrap();
+        let r_2to1 = r_1to2.opp();
         let bonds = gen_nonempty_bonds(rng);
-        gen.pattern[idx1].connections[r_1to2 as usize] = bonds;
-        gen.pattern[idx2].connections[r_2to1 as usize] = bonds;
+        gen.pattern[idx1].connections[r_1to2.to_usize()] = bonds;
+        gen.pattern[idx2].connections[r_2to1.to_usize()] = bonds;
         atom_disjoint_sets.union(idx1, idx2);
     }
 

src/nn.rs
@@ -411,6 +411,8 @@ impl<'a> SparseCoo1DSlice<'a> {
 }
 
 pub mod features {
+    use sim::Rot;
+
     use super::feature_offsets::*;
     use super::*;
 
@@ -530,8 +532,7 @@ pub mod features {
                 for (i, position) in positions.iter().enumerate() {
                     if let Some(mut features) = self.get_spatial_mut(*position) {
                         features.set(
-                            glyph_orientation
-                                .get_onehot_offset(sim::normalize_dir(glyph.rot) as usize),
+                            glyph_orientation.get_onehot_offset(glyph.rot.normalize().to_usize()),
                             1.,
                         );
                         use sim::GlyphType;
@@ -579,15 +580,15 @@ pub mod features {
                                 assert!(pos_list[i] == *position);
                                 if i >= 1 {
                                     let other_pos = positions[i - 1];
-                                    let rot = sim::pos_to_rot(other_pos - position).unwrap();
+                                    let rot = Rot::from_unit_pos(other_pos - position).unwrap();
                                     features
-                                        .set(track_minus_dir.get_onehot_offset(rot as usize), 1.);
+                                        .set(track_minus_dir.get_onehot_offset(rot.to_usize()), 1.);
                                 }
                                 if i + 1 < positions.len() {
                                     let other_pos = positions[i + 1];
-                                    let rot = sim::pos_to_rot(other_pos - position).unwrap();
+                                    let rot = Rot::from_unit_pos(other_pos - position).unwrap();
                                     features
-                                        .set(track_plus_dir.get_onehot_offset(rot as usize), 1.);
+                                        .set(track_plus_dir.get_onehot_offset(rot.to_usize()), 1.);
                                 }
                             }
                             GlyphType::Conduit(_, _) => unimplemented!(),
@@ -678,13 +679,13 @@ pub mod features {
                     if *arm_type == sim::ArmType::VanBerlo {
                         features.set(arm_base_is_berlo.get_offset(), 1.);
                     }
-                    for rel_r in (0..6).step_by(arm_type.angles_between_arm() as usize) {
-                        let abs_r = sim::normalize_dir(rel_r + rot);
-                        features.set(arm_in_orientation[abs_r as usize].get_offset(), 1.);
+                    for rel_r in Rot::step_by(arm_type.angles_between_arm()) {
+                        let abs_r = rel_r + rot.normalize();
+                        features.set(arm_in_orientation[abs_r.to_usize()].get_offset(), 1.);
                         use slotmap::Key;
-                        if !atoms_grabbed[rel_r as usize].is_null() {
+                        if !atoms_grabbed[rel_r.to_usize()].is_null() {
                             features.set(
-                                arm_in_orientation_and_holding_atom[abs_r as usize].get_offset(),
+                                arm_in_orientation_and_holding_atom[abs_r.to_usize()].get_offset(),
                                 1.,
                             );
                         }

src/parser.rs
@@ -207,7 +207,7 @@ struct Part {
     part_type: PartType,
     pos: Pos,
     arm_size: i32,
-    rot: i32,
+    rot: RawRot,
     input_output_index: i32,
     instructions: Vec<(i32, Instr)>,
     tracks: Option<Vec<Pos>>,
@@ -291,7 +291,7 @@ pub fn parse_solution(f: &mut impl Read) -> Result<FullSolution> {
         expect_u8(f, 1)?;
         let pos = parse_pos(f)?;
         let arm_size = parse_i32(f)?;
-        let rot = parse_i32(f)?;
+        let rot = RawRot(parse_i32(f)?);
         let input_output_index = parse_i32(f)?;
         let instruction_count = parse_i32(f)?;
         let mut instructions = Vec::new();
@@ -358,12 +358,17 @@ pub fn create_solution(
         let arm_size = 0;
         let arm_index = 0;
         let instructions = Vec::new();
-        //Clone and return points to relative position (absolute position done in setup_sim/reposition_glyph)
+        //Clone and return points to relative position (absolute position done in puzzle_prep)
         let (part_type, input_output_index, tracks, conduit) = match &glyph.glyph_type {
             GlyphType::Track(track) => (
                 PartType::Track,
                 0,
-                Some(track.iter().map(|x| rotate(x - pos, -rot)).collect()),
+                Some(
+                    track
+                        .iter()
+                        .map(|x| rotate(x - pos, -rot.normalize()))
+                        .collect(),
+                ),
                 None,
             ),
             GlyphType::Input(_, id) => (PartType::Input, *id, None, None),
@@ -373,7 +378,13 @@ pub fn create_solution(
                 PartType::Conduit,
                 *id,
                 None,
-                Some((*id, conduit.iter().map(|x| rotate(x - pos, -rot)).collect())),
+                Some((
+                    *id,
+                    conduit
+                        .iter()
+                        .map(|x| rotate(x - pos, -rot.normalize()))
+                        .collect(),
+                )),
             ),
             basic_glyph_type => (
                 PartType::Glyph(basic_glyph_type.try_into().unwrap()),
@@ -447,7 +458,7 @@ pub fn write_solution(f: &mut impl Write, sol: &FullSolution) -> Result<()> {
         write_u8(f, 1)?;
         write_pos(f, part.pos)?;
         write_i32(f, part.arm_size)?;
-        write_i32(f, part.rot)?;
+        write_i32(f, part.rot.0)?;
         write_i32(f, part.input_output_index)?;
         write_i32(f, part.instructions.len() as i32)?;
         for (instr_pos, instr) in &part.instructions {
@@ -512,10 +523,10 @@ fn parse_molecule(f: &mut impl Read) -> Result<AtomPattern> {
         let atom2 = *atom_locs
             .get(&to_pos)
             .ok_or(eyre!("bond2 to nonatom position"))?;
-        let rot =
-            pos_to_rot(pattern[atom2].pos - pattern[atom1].pos).ok_or(eyre!("nonadjacent bond"))?;
-        pattern[atom1].connections[rot as usize] = bond_type;
-        pattern[atom2].connections[normalize_dir(rot + 3) as usize] = bond_type;
+        let rot = Rot::from_unit_pos(pattern[atom2].pos - pattern[atom1].pos)
+            .ok_or(eyre!("nonadjacent bond"))?;
+        pattern[atom1].connections[rot.to_usize()] = bond_type;
+        pattern[atom2].connections[rot.opp().to_usize()] = bond_type;
     }
     Ok(pattern)
 }
@@ -530,17 +541,15 @@ fn write_molecule(f: &mut impl Write, pattern: &AtomPattern) -> Result<()> {
     }
     let mut bonds: HashMap<(Pos, Pos), Bonds> = HashMap::new();
     for atom1_idx in 0..pattern.len() {
-        for angle in 0..6 {
-            let bond_type = pattern[atom1_idx].connections[angle as usize];
+        for angle in Rot::ALL {
+            let bond_type = pattern[atom1_idx].connections[angle.to_usize()];
             if !bond_type.is_empty() {
                 let mut from_pos = pattern[atom1_idx].pos;
-                let mut to_pos = pattern[atom1_idx].pos + rot_to_pos(angle);
+                let mut to_pos = pattern[atom1_idx].pos + angle.to_pos();
                 let atom2_idx = *atom_locs
                     .get(&to_pos)
                     .ok_or(eyre!("bond to nonatom position"))?;
-                ensure!(
-                    pattern[atom2_idx].connections[normalize_dir(angle + 3) as usize] == bond_type
-                );
+                ensure!(pattern[atom2_idx].connections[angle.opp().to_usize()] == bond_type);
                 if (from_pos.x, from_pos.y) > (to_pos.x, to_pos.y) {
                     std::mem::swap(&mut from_pos, &mut to_pos);
                 }
@@ -561,7 +570,7 @@ fn write_molecule(f: &mut impl Write, pattern: &AtomPattern) -> Result<()> {
 }
 
 /// `input` needs to be in relative space (i.e. call this before
-/// `reposition_pattern`).
+/// `glyph.rot_by`, `glyph.move_by` etc).
 fn process_repeats(input: &Vec<Atom>, reps: i32) -> Result<AtomPattern> {
     let mut rep_offset = None;
     for atom in input {
@@ -709,11 +718,7 @@ pub fn puzzle_prep(puzzle: &FullPuzzle, soln: &FullSolution) -> Result<InitialWo
                     puzzle.inputs.len()
                 ))?;
                 let input_glyph = GlyphType::Input(pattern.clone(), p.input_output_index);
-                let mut glyph = Glyph {
-                    glyph_type: input_glyph,
-                    pos: Pos::zeros(),
-                    rot: 0,
-                };
+                let mut glyph = Glyph::new(input_glyph);
                 glyph.rot_by(p.rot);
                 glyph.move_by(p.pos);
                 glyphs.push(glyph);
@@ -730,11 +735,7 @@ pub fn puzzle_prep(puzzle: &FullPuzzle, soln: &FullSolution) -> Result<InitialWo
                     6 * puzzle.output_multiplier,
                     p.input_output_index,
                 );
-                let mut glyph = Glyph {
-                    glyph_type: output_glyph,
-                    pos: Pos::zeros(),
-                    rot: 0,
-                };
+                let mut glyph = Glyph::new(output_glyph);
                 glyph.rot_by(p.rot);
                 glyph.move_by(p.pos);
                 glyphs.push(glyph);
@@ -752,11 +753,7 @@ pub fn puzzle_prep(puzzle: &FullPuzzle, soln: &FullSolution) -> Result<InitialWo
                     6 * puzzle.output_multiplier,
                     p.input_output_index,
                 );
-                let mut glyph = Glyph {
-                    glyph_type: output_glyph,
-                    pos: Pos::zeros(),
-                    rot: 0,
-                };
+                let mut glyph = Glyph::new(output_glyph);
                 glyph.rot_by(p.rot);
                 glyph.move_by(p.pos);
                 glyphs.push(glyph);
@@ -766,11 +763,7 @@ pub fn puzzle_prep(puzzle: &FullPuzzle, soln: &FullSolution) -> Result<InitialWo
                     .tracks
                     .clone()
                     .ok_or(eyre!("Track data not found on track glyph"))?;
-                let mut glyph = Glyph {
-                    glyph_type: GlyphType::Track(track_locs),
-                    pos: Pos::zeros(),
-                    rot: 0,
-                };
+                let mut glyph = Glyph::new(GlyphType::Track(track_locs));
                 glyph.rot_by(p.rot);
                 glyph.move_by(p.pos);
                 glyphs.push(glyph);
@@ -780,11 +773,7 @@ pub fn puzzle_prep(puzzle: &FullPuzzle, soln: &FullSolution) -> Result<InitialWo
                     .conduit
                     .clone()
                     .ok_or(eyre!("Conduit data not found on conduit glyph"))?;
-                let mut glyph = Glyph {
-                    glyph_type: GlyphType::Conduit(conduit_locs, conduit_id),
-                    pos: Pos::zeros(),
-                    rot: 0,
-                };
+                let mut glyph = Glyph::new(GlyphType::Conduit(conduit_locs, conduit_id));
                 glyph.rot_by(p.rot);
                 glyph.move_by(p.pos);
                 glyphs.push(glyph);

commit 7c1ce7d53b40d09aebb17038ea81f5dd325cb976

fix regression caused by fbb7916e42f880a056c026e4e9ab1bb6e48fbbd3 -- I have no idea why check didn't catch this

src/render_sim.rs
@@ -631,7 +631,7 @@ impl RenderDataBase {
             let offset = [f_arm.pos.x, f_arm.pos.y];
             let triangles_drawn = if f_arm.grabbing { 6 } else { 3 };
             for r in Rot::step_by(f_arm.arm_type.angles_between_arm()) {
-                let angle = f_arm.rot + rot_to_angle(r.raw());
+                let angle = f_arm.rot + rot_to_angle(r.raw_nonneg());
                 ctx.apply_uniforms(UniformsSource::table(&BasicUniforms {
                     color,
                     offset,

src/revgen.rs
@@ -565,7 +565,7 @@ impl GenState {
                 let arm_idx = self.world.arms.len();
                 self.world
                     .arms
-                    .push(Arm::new(arm_pos, arm_rot.raw(), arm_len, ArmType::PlainArm));
+                    .push(Arm::new(arm_pos, arm_rot.raw_minabs(), arm_len, ArmType::PlainArm));
                 self.tapes.push(Tape {
                     first: 0,
                     instructions: Vec::new(),

src/sim.rs
@@ -376,8 +376,14 @@ impl Rot {
         (0..6).step_by(step).map(Rot)
     }
 
-    pub fn raw(self) -> RawRot {
+    /// Returns a value in the range `0..6`
+    pub fn raw_nonneg(self) -> RawRot {
         RawRot(self.0 as i32)
+    }
+
+    /// Returns a value in the range `-3..3`
+    pub fn raw_minabs(self) -> RawRot {
+        RawRot(self.to_i32_minabs())
     }
 
     pub fn from_i8(value: i8) -> Self {
@@ -902,7 +908,7 @@ impl Arm {
         use Movement::*;
         match action {
             Move(Linear(p)) => self.pos += p,
-            Move(Rotation(r, _)) => self.rot += r.to_i32_minabs(),
+            Move(Rotation(r, _)) => self.rot += r.raw_minabs(),
             LengthAdjust(a) => self.len += a,
             Move(HeldStill) | Pivot(_) => {}
         }
@@ -1352,7 +1358,7 @@ impl FloatWorld {
                 Rotation(r, pivot) => {
                     let pivot_xy = pos_to_xy(pivot);
                     let offset = xy - pivot_xy;
-                    let rot = rot_to_angle(r.raw()) * amount;
+                    let rot = rot_to_angle(r.raw_minabs()) * amount;
                     (pivot_xy + (nalgebra::Rotation2::new(rot) * offset), rot)
                 }
                 HeldStill => (xy, 0.),
@@ -2136,7 +2142,7 @@ impl World {
                 "atom/arm collision!",
             )?;
             for r in Rot::step_by(arm.arm_type.angles_between_arm()) {
-                let angle = arm.rot + rot_to_angle(r.raw());
+                let angle = arm.rot + rot_to_angle(r.raw_nonneg());
                 let offset = nalgebra::Rotation2::new(angle) * XYVec::new(arm.len, 0.);
                 mark_point(&mut self.area_touched, arm.pos + offset);
                 //arm lengths are doubled in floatworld
@@ -2381,7 +2387,7 @@ impl World {
                     pair_data.vecids.1 = id;
                     unfinished_conduit_count -= 1;
                     pair_data.offset_pos -= glyph.pos;
-                    pair_data.offset_rot = (pair_data.offset_rot - glyph.rot).normalize().raw();
+                    pair_data.offset_rot = (pair_data.offset_rot - glyph.rot).normalize().raw_nonneg();
                 } else {
                     let conduit_info = ConduitInfo {
                         vecids: (id, id),
@@ -2710,7 +2716,9 @@ mod tests {
         let r = Rot(1);
         let rp = rotate(p, r);
         let frp = pos_to_xy(rp);
-        let rfp = nalgebra::Rotation2::new(rot_to_angle(r.raw())) * fp;
+        let rfp = nalgebra::Rotation2::new(rot_to_angle(r.raw_minabs())) * fp;
+        assert!(nalgebra::distance(&frp, &rfp) < 1e-6);
+        let rfp = nalgebra::Rotation2::new(rot_to_angle(r.raw_nonneg())) * fp;
         assert!(nalgebra::distance(&frp, &rfp) < 1e-6);
 
         for r in Rot::ALL {